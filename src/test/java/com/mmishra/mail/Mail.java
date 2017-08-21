64046eb5ccaafde7c63fbc846f2b191f36f38f31
mmishra77790 <31176605+mmishra77790@users.noreply.github.com>
2017-08-21 23:46:13 +0530

    Add files via upload

---
+++ src/test/java/com/mmishra/mail/Mail.java
@@ -0,0 +1,27 @@
+package com.mmishra.mail;
+
+public class Mail {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Sent Mail..");
+	}
+
+}
